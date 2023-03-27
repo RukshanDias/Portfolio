65126bc603d2f8849fd17cadf8730c9ab1af710e
RukshanDias <rukshandias002@gmail.com>
2023-03-27 07:17:58 +0530

    add about section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
 import Home from "./components/home/Home";
+import About from "./components/about/About";
 import Contact from "./components/contact/Contact";
 import Projects from "./components/projects/Projects";
 
@@ -10,6 +11,7 @@ function App() {
         <div className="App">
             <Navbar />
             <Home />
+            <About />
             <Projects />
             <Contact />
         </div>
